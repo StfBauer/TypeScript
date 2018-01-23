06bf2c81d7cba2ed3dee2119c16fbd8baa4ab1f1
Ron Buckton <ron.buckton@microsoft.com>
2018-01-23 10:40:56 -0800

    Update build script

--- jenkins.sh
+++ jenkins.sh
@@ -13,8 +13,12 @@ npm install
 # Node 6 uses an older version of npm that does not symlink a package with a "file:" reference
 if [ "$1" = "6" ]; then
     npm uninstall typemock @typescript/vfs @typescript/vfs-path @typescript/vfs-core @typescript/vfs-errors --no-save;
+    ln -s node_modules/typemock scripts/typemock;
+    ln -s node_modules/@typescript/vfs-errors scripts/vfs-errors;
+    ln -s node_modules/@typescript/vfs-core scripts/vfs-core;
+    ln -s node_modules/@typescript/vfs-path scripts/vfs-path;
+    ln -s node_modules/@typescript/vfs scripts/vfs;
     npm run build:private-packages;
-    npm install file:scripts/typemock file:scripts/vfs file:scripts/vfs-path --no-save;
 fi
 
 npm update
